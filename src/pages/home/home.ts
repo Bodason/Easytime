f0ab4fb71061770920c841737c48db26711f9e8f
bodason <tbo@systemate.dk>
2019-07-24 18:36:44 +0200

    set platform list on home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
+import { Platform } from 'ionic-angular/platform/platform';
 import { Pages } from './../../enums/pages';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
-import { Platform } from 'ionic-angular';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -14,14 +14,14 @@ export class HomePage {
   pageName = Pages.Home;
   isCordova: boolean;
   callback: string;
-
+  platform: any;
   formGroup: FormGroup;
 
   constructor(
     public navCtrl: NavController, 
-
+    public platformProvider: Platform
     ) {
-
+      this.platform = this.platformProvider.platforms();
   }
 
 
